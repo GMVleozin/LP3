5dcec97728bb88e79cef1219eff9a2c955b1e27f
GMVleozin <leonardovso265686@gmail.com>
2026-03-05 17:38:16 -0300

    Add files via upload

---
+++ lp3_aula_6/recebaDados.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php
+
+    $nome = $_POST['txtNome'];
+    $email = $_POST['txtEmail'];
+    $desc = $_POST['txtDesc'];
+
+    echo "$nome<br>";
+    echo "$email<br>";
+    echo "$desc<br>";
+    ?>
+
+    <a href="index.php" class="btn btn-outline-success" role="button">Voltar</a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
